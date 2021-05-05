b8a20cfaad876974a05f82bb8861a43cb06e7d1b
TitaniaTendean <s21710599@student.unklab.ac.id>
2021-05-05 09:06:08 +0700

    Removed Previous Commit

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -6,11 +6,6 @@ function About() {
     <div className="container-sm mb-4">
       <NavBar />
       <h3>About</h3>
-
-      <h4>Nama : Tarisha Rehuel Jenena Hamzah</h4>
-      <h4>NIM : 105021810046 / S21810347</h4>
-      <h4>Do not coppy this excercise please</h4>
-
     </div>
   );
 }
